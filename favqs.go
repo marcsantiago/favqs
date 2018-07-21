da35b7dd133c0d184ad922b5528373dda49321c0
Marc Santiago <marcanthonysanti@gmail.com>
2018-07-20 23:41:29 -0400

    Favqs: Adding more functionality + Dep

--- favqs.go
+++ favqs.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -26,6 +27,41 @@ var (
 	// ErrSessionFailed ...
 	ErrSessionFailed = fmt.Errorf("Session creation failed")
 )
+
+var (
+	// DefaultFilter provides a easy defualt filter for my use case
+	DefaultFilter = "fashion"
+	// DefaultMax provides the number of quotes to return for my use case
+	DefaultMax = 10
+)
+
+// QuoteOfTheDate response structure from favq
+type QuoteOfTheDate struct {
+	QotdDate string `json:"qotd_date"`
+	Quote    Quote  `json:"quote"`
+}
+
+// Quotes returns a paged list of quotes from favq
+type Quotes struct {
+	Page     int     `json:"page"`
+	LastPage bool    `json:"last_page"`
+	Quotes   []Quote `json:"quotes"`
+}
+
+// Quote base structure
+type Quote struct {
+	ID              int      `json:"id,omitempty"`
+	FavoritesCount  int      `json:"favorites_count,omitempty"`
+	Dialogue        bool     `json:"dialogue,omitempty"`
+	Favorite        bool     `json:"favorite,omitempty"`
+	Tags            []string `json:"tags,omitempty"`
+	URL             string   `json:"url,omitempty"`
+	UpvotesCount    int      `json:"upvotes_count,omitempty"`
+	DownvotesCount  int      `json:"downvotes_count,omitempty"`
+	Author          string   `json:"author,omitempty"`
+	AuthorPermalink string   `json:"author_permalink,omitempty"`
+	Body            string   `json:"body,omitempty"`
+}
 
 // Client a wrapper favqs
 type Client struct {
@@ -106,6 +142,74 @@ func New() (Client, error) {
 }
 
 // GetQuoteOfTheDay ...
-func (c Client) GetQuoteOfTheDay() string {
-	return ""
+func (c Client) GetQuoteOfTheDay() (QuoteOfTheDate, error) {
+	var quote QuoteOfTheDate
+	req, err := http.NewRequest("GET", baseURL+"qotd", nil)
+	if err != nil {
+		return quote, err
+	}
+	// not required, but might has well use it since it's already present
+	req.Header = c.authHeaders
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return quote, err
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return quote, err
+	}
+
+	err = json.Unmarshal(b, &quote)
+	if err != nil {
+		return quote, err
+	}
+
+	return quote, nil
+}
+
+// GetQuotes returns quotes from filter, defaults to tag type
+func (c Client) GetQuotes(filter string, max int) ([]Quote, error) {
+	u, err := url.Parse(baseURL + "quotes")
+	if err != nil {
+		return nil, err
+	}
+	params := u.Query()
+	params.Add("filter", filter)
+	params.Add("type", "tag")
+	u.RawQuery = params.Encode()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = c.authHeaders
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var tqs Quotes
+	err = json.Unmarshal(b, &tqs)
+	if err != nil {
+		return nil, err
+	}
+
+	quotes := make([]Quote, max)
+	for i, q := range tqs.Quotes {
+		if i < len(tqs.Quotes) {
+			quotes[i] = q
+		}
+	}
+
+	return quotes, nil
 }
